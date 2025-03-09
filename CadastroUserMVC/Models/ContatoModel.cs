81fde938080b515d292dfe27dc25104930ba19ed
carlospenaforte <carlosedu_1132@hotmail.com>
2025-03-08 22:23:52 -0300

    option for default connection

--- CadastroUserMVC/Models/ContatoModel.cs
+++ CadastroUserMVC/Models/ContatoModel.cs
@@ -6,5 +6,6 @@
         public string Nome { get; set; }
         public string Email { get; set; }
         public string Celular { get; set; }
+        public int ContatoID { get; set; }
     }
 }
